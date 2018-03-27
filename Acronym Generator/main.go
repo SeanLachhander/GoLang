0de8f45e35fdd55a6a8c6ff2fa3af3e9cdce46b9
Sean Lachhander, MS <seanlachhander@gmail.com>
2018-03-27 00:37:07 -0400

    Update main.go
    For reference later to understand bufio import pkg

--- Acronym Generator/main.go
+++ Acronym Generator/main.go
@@ -3,7 +3,7 @@ package main
 import(
 	"fmt"
 	"strings"
-	"bufio"
+	"bufio" // implements buffered I/O. It wraps an io.Reader or io.Writer object, creating another object (Reader or Writer) that also implements the interface but provides buffering and some help for textual I/O.
 	"os"
 )
 
